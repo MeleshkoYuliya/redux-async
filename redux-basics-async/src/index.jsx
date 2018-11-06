648e2757fb464306906e9b6553ec8902c4e97443
jULIAmEleshk <39305791+jULIAmEleshk@users.noreply.github.com>
2018-11-06 17:30:42 +0300

    Add files via upload

---
+++ redux-basics-async/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { getTodos } from './actions';
+
+import store from './store';
+import App from './App';
+
+store.dispatch(getTodos());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
